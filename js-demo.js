622cbcc74b41556ab9d88f1bacda70dfed3fd0de
prof-amartin <100564055+prof-amartin@users.noreply.github.com>
2023-04-11 13:22:44 -0700

    Add files via upload

---
+++ js-demo.js
@@ -0,0 +1,9 @@
+﻿/*
+   JS Activity Clock
+   Name: 
+   Date:   
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
